de16dd09e529d88eea49b16eb0216328567cbb52
Md. Nahiduzzaman <nahid.cse.diu.19@gmail.com>
2025-03-18 12:54:50 +0600

    added process section

--- app/pages/Home/UserHome.tsx
+++ app/pages/Home/UserHome.tsx
@@ -3,6 +3,7 @@ import HomeBanner from "@/app/components/HomeBanner";
 import HomeFunFact from "@/app/components/HomeFunFact";
 import HomeHeroSection from "@/app/components/HomeHeroSection";
 import ServicesSection from "@/app/components/HomeOurService";
+import HomeProcessSection from "@/app/components/HomeProcessSection";
 import TrustedBrands from "@/app/components/TrustedBrands";
 import React from "react";
 
@@ -12,6 +13,7 @@ export default function UserHome() {
      <HomeBanner></HomeBanner>
      <TrustedBrands></TrustedBrands>
      <HomeHeroSection></HomeHeroSection>
+     <HomeProcessSection></HomeProcessSection>
      <ServicesSection></ServicesSection>
      <HomeFunFact></HomeFunFact>
     </div>
